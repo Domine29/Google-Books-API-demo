5438e5c8b088b1876ce749392380e84a0c2281a3
domine29 <eric.dominguez29@gmail.com>
2022-12-19 09:11:29 -0600

    created interface. Working on Google Books API.

---
+++ reading_list.py
@@ -0,0 +1,11 @@
+while True: 
+    mode = input("\n== Reading List ==\n1. Search Books\n2. Reading List\n3. Exit\n")
+
+    if mode == '1':
+        pass
+
+    elif mode == '2':
+        pass
+
+    elif mode == '3':
+        break
